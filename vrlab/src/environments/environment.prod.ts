60cd78daa76b6e7b562af7aadcbab3727dae239a
Weiyu Feng <47650296+0WeiyuFeng0@users.noreply.github.com>
2020-09-08 18:18:42 -0500

    deploy to firebase
    Get firebase hosting worrk.

---
+++ vrlab/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  apiKey: "AIzaSyAHJdlNqZeDKyAmYWgxqiSgiJvNmz_k8FA",
+  authDomain: "vrlabtraining-ccc08.firebaseapp.com",
+  databaseURL: "https://vrlabtraining-ccc08.firebaseio.com",
+  projectId: "vrlabtraining-ccc08",
+  storageBucket: "vrlabtraining-ccc08.appspot.com",
+  messagingSenderId: "141126789009",
+  appId: "1:141126789009:web:b1062d7ba27ef3373c5c30",
+  measurementId: "G-8BHJGCDMDC"
+};
